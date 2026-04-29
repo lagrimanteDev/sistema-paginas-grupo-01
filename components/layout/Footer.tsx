04e640e2a1d723d87e4bd53955ae173c613dc5bb
Hugo Lagrimante Teixeira <hlagrimante@gmail.com>
2026-04-28 21:31:30 -0300

    chore: adiciona meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+  { nome: "Hugo Lagrimante Teixeira", github: "lagrimanteDev" },
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
